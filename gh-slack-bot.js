997bf3cac49d9331204c0c2fcdd31b47583fae18
Whelpley <whelpley@gmail.com>
2016-10-05 11:27:54 -0400

    Roll back to old Send

--- gh-slack-bot.js
+++ gh-slack-bot.js
@@ -19,35 +19,9 @@ module.exports = function (req, res, next) {
 }
 
 // old send
-// function send (payload, callback) {
-//   var path = process.env.INCOMING_WEBHOOK_PATH;
-//   var uri = 'https://hooks.slack.com/services/' + path;
-
-//   request({
-//     uri: uri,
-//     method: 'POST',
-//     body: JSON.stringify(payload)
-//   }, function (error, response, body) {
-//     if (error) {
-//       return callback(error);
-//     }
-//     callback(null, response.statusCode, body);
-//   });
-// }
-
-// new send
-function send (payload) {
+function send (payload, callback) {
   var path = process.env.INCOMING_WEBHOOK_PATH;
   var uri = 'https://hooks.slack.com/services/' + path;
-  var callback = function (error, status, body) {
-      if (error) {
-        return next(error);
-      } else if (status !== 200) {
-        return next(new Error('Incoming WebHook: ' + status + ' ' + body));
-      } else {
-        return res.status(200).end();
-      }
-    };
 
   request({
     uri: uri,
@@ -60,6 +34,32 @@ function send (payload) {
     callback(null, response.statusCode, body);
   });
 }
+
+// new send
+// function send (payload) {
+//   var path = process.env.INCOMING_WEBHOOK_PATH;
+//   var uri = 'https://hooks.slack.com/services/' + path;
+//   var callback = function (error, status, body) {
+//       if (error) {
+//         return next(error);
+//       } else if (status !== 200) {
+//         return next(new Error('Incoming WebHook: ' + status + ' ' + body));
+//       } else {
+//         return res.status(200).end();
+//       }
+//     };
+
+//   request({
+//     uri: uri,
+//     method: 'POST',
+//     body: JSON.stringify(payload)
+//   }, function (error, response, body) {
+//     if (error) {
+//       return callback(error);
+//     }
+//     callback(null, response.statusCode, body);
+//   });
+// }
 
 
 
@@ -234,17 +234,17 @@ function prepareQuestionsPayload(questions, botPayload, res) {
     // });
 
 // old send
-    // send(botPayload, function (error, status, body) {
-    //   if (error) {
-    //     return next(error);
-    //   } else if (status !== 200) {
-    //     return next(new Error('Incoming WebHook: ' + status + ' ' + body));
-    //   } else {
-    //     return res.status(200).end();
-    //   }
-    // });
+    send(botPayload, function (error, status, body) {
+      if (error) {
+        return next(error);
+      } else if (status !== 200) {
+        return next(new Error('Incoming WebHook: ' + status + ' ' + body));
+      } else {
+        return res.status(200).end();
+      }
+    });
 // new send
-    send(botPayload);
+    // send(botPayload);
 };
 
 function prepareCompaniesPayload(companies, botPayload, res) {
