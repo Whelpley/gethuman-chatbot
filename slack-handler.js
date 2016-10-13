006d31ea7089ad20dd39fb17ed12b3af933db6ee
Whelpley <whelpley@gmail.com>
2016-10-13 09:19:39 -0400

    Cull search for Guides API

--- slack-handler.js
+++ slack-handler.js
@@ -4,7 +4,6 @@ const request = require('request'),
     Q = require('q'),
     companySearch = require('./api/company.js'),
     postSearch = require('./api/post.js'),
-    guideSearch = require('./api/guide.js'),
     preparePayload = require('./api/payloads-slack.js');
 
 module.exports = {
@@ -49,13 +48,12 @@ function getResponsePayload(platformRequestContext) {
 
       if (posts && posts.length) {
         // is it a bad idea to have a nested .then?
-          return attachCompaniesAndGuides(posts)
+          return attachCompaniesToPosts(posts)
             .then(function (posts){
                 console.log("About to prepare payload from Posts object: " + JSON.stringify(posts).substring(0,200));
                 result.data = preparePayload.posts(posts);
                 result.raw = result.data;
                 console.log("Payload prepared by slack handler for POSTS: " + JSON.stringify(result));
-                // goes to here fine !!
                 return result;
             });
       }
@@ -63,14 +61,12 @@ function getResponsePayload(platformRequestContext) {
           result.data = preparePayload.companies(companies);
           result.raw = result.data;
           console.log("Payload prepared by slack handler for COMPANIES: " + JSON.stringify(result));
-          // goes to here fine!
           return result;
       }
       else {
           result.data = preparePayload.nothingFound();
           result.raw = result.data;
           console.log("Payload prepared by slack handler for NOTHING FOUND: " + JSON.stringify(result));
-          // goes to here fine!
           return result;
       }
   })
@@ -118,40 +114,26 @@ function sendErrorResponse(errorPayload) {
 
 //  ---------- Helper Methods ----------------
 
-// access Company and Guide objects matching each Question, return one mega-Posts object
-//  !!!! WE DONT EVEN NEED THE GUIDES - ON THE CHOPPING BLOCK !!!!
 // method could also be refactored
-function attachCompaniesAndGuides(posts) {
-    // console.log("About to attach C and G to Posts.");
+function attachCompaniesToPosts(posts) {
+    console.log("About to attach Companies to Posts.");
     var companyIDs = [];
-    var guideIDs = [];
     for (let i = 0; i < posts.length; i++) {
         companyIDs.push(posts[i].companyId);
-        guideIDs.push(posts[i].guideId);
     };
-
-    return Q.all([
-            companySearch.findByIds(companyIDs),
-            guideSearch.findByIds(guideIDs)
-        ])
-    .then(function (companiesAndGuides) {
-        var companies = companiesAndGuides[0];
-        var guides = companiesAndGuides[1];
+    return Q.when(companySearch.findByIds(companyIDs))
+    .then(function (companies) {
         // refactor this
         var companyTable = {};
-        var guideTable = {};
         // do I need to translate these to map/forEach operations for async?
         for (let i = 0; i < companies.length; i++) {
             companyTable[companies[i]._id] = companies[i];
-            guideTable[guides[i]._id] = guides[i];
         };
         for (let i = 0; i < posts.length; i++) {
             let cID = posts[i].companyId;
             posts[i].company = companyTable[cID];
-            let gID = posts[i].guideId;
-            posts[i].guide = guideTable[gID];
         };
-        // console.log("About to return Posts after attaching C and G: " + JSON.stringify(posts).substring(0,200));
+        console.log("About to return Posts after attaching Companies: " + JSON.stringify(posts).substring(0,400));
         return posts;
     })
 }
