0b87effaebd72d92fc337810dc2f3021d3d316fd
Whelpley <whelpley@gmail.com>
2016-10-14 14:03:08 -0400

    debug

--- src/bot-handlers/messenger-handler.js
+++ src/bot-handlers/messenger-handler.js
@@ -8,8 +8,8 @@
 // need to examine the request coming from Facebook for what distinguishes it
 // basically returns False now
 function isHandlerForRequest(platformRequestContext) {
-  var responseUrl = platformRequestContext.userRequest.response_url || '';
-  return (responseUrl && responseUrl.includes('facebook')) ? true : false;
+  var object = platformRequestContext.userRequest.object || '';
+  return (object === 'page') ? true : false;
 }
 
 function getResponsePayload() {
