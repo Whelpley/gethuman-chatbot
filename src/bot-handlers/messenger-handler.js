fa7470d7ec86736fc86fd3096238f482bdab64b1
Whelpley <whelpley@gmail.com>
2016-10-14 15:55:22 -0400

    Rough version of FB bot

--- src/bot-handlers/messenger-handler.js
+++ src/bot-handlers/messenger-handler.js
@@ -1,6 +1,11 @@
 'use strict'
 
 const token = process.env.FB_PAGE_ACCESS_TOKEN
+const request = require('request');
+const Q = require('q');
+const companySearch = require('../api-gh/company.js');
+const postSearch = require('../api-gh/post.js');
+const preparePayload = require('./messenger-payload.js');
 
 function isHandlerForRequest(context) {
   var object = context.userRequest.object || '';
@@ -14,10 +19,35 @@ function getResponsePayload(context) {
     let event = context.userRequest.entry[0].messaging[i]
     let sender = event.sender.id
 
-    // handling text input
     if (event.message && event.message.text) {
-        let textInput = event.message.text;
-        console.log("Text input from user: " + textInput);
+      let textInput = event.message.text;
+      console.log("Text input received from user: " + textInput);
+      var payload = {
+        raw: {},
+        data:  {},
+        context: context
+      };
+      return Q.all([
+          postSearch.findByText(textInput),
+          companySearch.findByText(textInput)
+      ])
+      .then(function (postAndCompanySearchResults) {
+        console.log('Initial Post and Company searches complete, about to load payload object from search results');
+        var posts = postAndCompanySearchResults[0];
+        var companies = postAndCompanySearchResults[1];
+        if (posts && posts.length) {
+          console.log("It's going to be a Posts message return");
+          return preparePayload.addPostsToPayload(payload, posts);
+        }
+        else if (companies && companies.length) {
+          console.log("It's going to be a Companies message return");
+          return preparePayload.addCompaniesToPayload(payload, companies);
+        }
+        else {
+          console.log("It's going to be a Nothing Found message return");
+          return preparePayload.nothingFound(payload);
+        }
+      });
     }
   }
 
@@ -25,38 +55,40 @@ function getResponsePayload(context) {
 }
 
 
-  // var payload = {
-  //   raw: {},
-  //   data:  {},
-  //   context: context
-  // }
-  // if (!textInput) {
-  //     return Q.when(preparePayload.inputPrompt(payload));
-  // }
-  // console.log('About to search API for input: ' + textInput);
-  // return Q.all([
-  //     postSearch.findByText(textInput),
-  //     companySearch.findByText(textInput)
-  // ])
-  // .then(function (postAndCompanySearchResults) {
-  //   console.log('About to load payload object from search results');
-  //   var posts = postAndCompanySearchResults[0];
-  //   var companies = postAndCompanySearchResults[1];
-
-  //   if (posts && posts.length) {
-  //     return preparePayload.addPostsToPayload(payload, posts);
-  //   }
-  //   else if (companies && companies.length) {
-  //     return preparePayload.addCompaniesToPayload(payload, companies);
-  //   }
-  //   else {
-  //     return preparePayload.nothingFound(payload);
-  //   }
-  // });
 
 
-function sendResponseToPlatform() {
-  console.log('Hit the sendResponseToPlatform Function');
+
+function sendResponseToPlatform(responsePayload) {
+  console.log('Hitting the sendResponseToPlatform function with this payload: ' + JSON.stringify(responsePayload));
+  var elements = responsePayload.data;
+  var event = responsePayload.userRequest.entry[0].messaging[i];
+  var sender = event.sender.id;
+
+// gotta make this a Promise
+  request({
+      url: 'https://graph.facebook.com/v2.6/me/messages',
+      qs: {access_token: token},
+      method: 'POST',
+      json: {
+          recipient: {id: sender},
+          message: {
+              "attachment": {
+                  "type": "template",
+                  "payload": {
+                      "template_type": "generic",
+                      "elements": elements
+                  }
+              }
+          },
+      }
+  }, function(error, response, body) {
+      if (error) {
+          console.log('Error sending messages: ', error)
+      } else if (response.body.error) {
+          console.log('Error: ', response.body.error)
+      }
+  });
+
 }
 
 
---
+++ src/bot-handlers/messenger-payload.js
@@ -0,0 +1,147 @@
+// prepares payloads for FB Messenger responses
+
+'use strict'
+
+const Q = require('q');
+const companySearch = require('../api-gh/company.js');
+const phoneFormatter = require('phone-formatter');
+
+function addPostsToPayload(payload, posts) {
+  return queryCompaniesOfPosts(posts)
+    .then(function (posts){
+        payload.data = preparePostsPayload(posts);
+        payload.raw = payload.data;
+        return payload;
+    });
+}
+
+function addCompaniesToPayload(payload, companies) {
+    payload.data = prepareCompaniesPayload(companies);
+    payload.raw = payload.data;
+    return payload;
+}
+
+function nothingFound(payload) {
+    // payload.data.username = 'Gethuman Bot';
+    // payload.data.text = "We could not find anything matching your input to our database. Could you try rephrasing your concern, and be sure to spell the company name correctly?";
+    // payload.data.icon_emoji = ':question:';
+    // payload.raw = payload.data;
+    // return payload;
+};
+
+function error(error) {
+    // var payload = {};
+    // payload.username = 'Gethuman Bot';
+    // payload.text = error;
+    // payload.icon_emoji = ':no_good:';
+    // return payload;
+};
+
+// ----- helper methods
+
+
+function queryCompaniesOfPosts(posts) {
+    var companyIDs = [];
+    for (let i = 0; i < posts.length; i++) {
+        companyIDs.push(posts[i].companyId);
+    };
+    return Q.when(companySearch.findByIds(companyIDs))
+      .then(function (companies) {
+        var companyTable = {};
+        for (let i = 0; i < companies.length; i++) {
+            companyTable[companies[i]._id] = companies[i];
+        };
+        for (let i = 0; i < posts.length; i++) {
+            let cID = posts[i].companyId;
+            posts[i].company = companyTable[cID];
+        };
+        return posts;
+    })
+}
+
+function preparePostsPayload(posts) {
+  console.log("Hitting the preparePostsPayload function with these posts: " + JSON.stringify(posts).substring(0,200));
+
+    let elements = [];
+    for (let i = 0; i < questions.length; i++) {
+        let companyName = questions[i].companyName || '';
+        let urlId = questions[i].urlId || '';
+        let phone = (questions[i].company) ? questions[i].company.callback.phone : '';
+        let phoneIntl = (phone) ? phoneFormatter.format(phone, "+1NNNNNNNNNN") : '';
+        let title = questions[i].title || '';
+        if (title.indexOf(companyName) < 0) {
+            title = companyName + ": " + title;
+        };
+        let textField = extractTextFieldFromPost(posts[i]);
+
+        let singleElement = {
+            "title": title,
+            "subtitle": textField,
+            "buttons": [{
+                "type": "web_url",
+                "url": "https://answers.gethuman.co/_" + encodeURIComponent(urlId) ,
+                "title": "Step by Step Guide"
+            }, {
+                "type": "web_url",
+                "url": "https://gethuman.com?company=" + encodeURIComponent(companyName) ,
+                "title": "Solve for Me - $20"
+            }],
+        };
+        // if there is a valid phone # (needs stricter checks), add Call button
+        if (phoneIntl) {
+            singleElement.buttons.unshift({
+                "type": "phone_number",
+                "title": "Call " + companyName,
+                "payload": phoneIntl
+            })
+        };
+        elements.push(singleElement);
+    };
+    console.log("Ermagerd we made some Elements: " + JSON.stringify(elements));
+    return elements
+}
+
+function prepareCompaniesPayload(companies) {
+  console.log("Hitting the prepareCompaniesPayload function with these companies: " + JSON.stringify(companies).substring(0,200));
+}
+
+// Following methods are duplicated: send to another module & share!
+
+function extractTextFieldFromPost(post) {
+    let phone = (post.company) ? post.company.callback.phone : '';
+    let emailContactMethods = post.company.contactMethods.filter(function (method) {
+        return method.type === "email";
+    });
+    let email = (emailContactMethods && emailContactMethods.length) ? emailContactMethods[0].target : '';
+    return formatTextField(phone, email);
+}
+
+function extractTextFieldFromCompany(company) {
+    let phone = company.callback.phone || '';
+    let emailContactMethods = company.contactMethods.filter(function (method) {
+        return method.type === "email";
+    });
+    let email = (emailContactMethods && emailContactMethods.length) ? emailContactMethods[0].target : '';
+    return formatTextField(phone, email);
+}
+
+function formatTextField(phone, email) {
+    let result = '';
+    if (phone && email) {
+        result = phone + " | " + email;
+    } else if (phone) {
+        result = phone;
+    } else if (email) {
+        result = email;
+    };
+    return result;
+};
+
+// ----------------------------------
+
+module.exports = {
+  addPostsToPayload: addPostsToPayload,
+  addCompaniesToPayload: addCompaniesToPayload,
+  nothingFound: nothingFound,
+  error: error
+}
