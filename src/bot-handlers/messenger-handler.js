2bb06a3007038ef016e177efc5cc3cd1efb1f573
Whelpley <whelpley@gmail.com>
2016-10-14 14:30:46 -0400

    Slow rebuild of FB bot

--- src/bot-handlers/messenger-handler.js
+++ src/bot-handlers/messenger-handler.js
@@ -1,20 +1,62 @@
 'use strict'
 
-// should this just be declared in FB bot module?
-// const token = process.env.FB_PAGE_ACCESS_TOKEN
+const token = process.env.FB_PAGE_ACCESS_TOKEN
 
-
-
-// need to examine the request coming from Facebook for what distinguishes it
-// basically returns False now
 function isHandlerForRequest(platformRequestContext) {
   var object = platformRequestContext.userRequest.object || '';
   return (object === 'page') ? true : false;
 }
 
-function getResponsePayload() {
+function getResponsePayload(context) {
+  var messaging_events = context.userRequest.entry[0].messaging;
+
+  for (let i = 0; i < messaging_events.length; i++) {
+    let event = context.userRequest.entry[0].messaging[i]
+    let sender = event.sender.id
+
+    // handling text input
+    if (event.message && event.message.text) {
+        let textInput = event.message.text;
+        console.log("Text input from user: " + textInput);
+    }
+  }
+
+  context.finishResponse;
+ }
+
+
+  // var payload = {
+  //   raw: {},
+  //   data:  {},
+  //   context: context
+  // }
+  // if (!textInput) {
+  //     return Q.when(preparePayload.inputPrompt(payload));
+  // }
+  // console.log('About to search API for input: ' + textInput);
+  // return Q.all([
+  //     postSearch.findByText(textInput),
+  //     companySearch.findByText(textInput)
+  // ])
+  // .then(function (postAndCompanySearchResults) {
+  //   console.log('About to load payload object from search results');
+  //   var posts = postAndCompanySearchResults[0];
+  //   var companies = postAndCompanySearchResults[1];
+
+  //   if (posts && posts.length) {
+  //     return preparePayload.addPostsToPayload(payload, posts);
+  //   }
+  //   else if (companies && companies.length) {
+  //     return preparePayload.addCompaniesToPayload(payload, companies);
+  //   }
+  //   else {
+  //     return preparePayload.nothingFound(payload);
+  //   }
+  // });
+
 
 }
+
 
 function sendResponseToPlatform() {
 
