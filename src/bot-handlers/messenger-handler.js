ba65d2284af8dc982905da70c2487fe833a1cf10
Whelpley <whelpley@gmail.com>
2016-10-26 12:12:33 -0400

    debug

--- src/bot-handlers/messenger-handler.js
+++ src/bot-handlers/messenger-handler.js
@@ -32,83 +32,83 @@ function getResponseObj(context) {
       let textInput = event.message.text;
       responseObj.context.textInput = textInput;
       console.log("Text input received from user: " + textInput);
-      // summonResponse(responseObj, textInput);
-        return Q.when(companySearch.findAllByText(textInput))
-        .then(function (companySearchResults) {
-          // console.log("Company Search Results: " + JSON.stringify(companySearchResults).substring(0,200));
-          var company = {};
+      return summonResponse(responseObj, textInput);
+        // return Q.when(companySearch.findAllByText(textInput))
+        // .then(function (companySearchResults) {
+        //   // console.log("Company Search Results: " + JSON.stringify(companySearchResults).substring(0,200));
+        //   var company = {};
 
-          // separate out this as function - duplicated in all bots
-          var exactMatch = companySearchResults.filter(function(eachCompany) {
-            return eachCompany.name.toLowerCase() === textInput.toLowerCase();
-          });
-          if (!companySearchResults.length) {
-            console.log("Nothing found in initial Company search");
-            return prepareResponse.nothingFound(responseObj);
-          }
-          else if (exactMatch && exactMatch.length) {
-            company = exactMatch[0];
-            console.log("Found an exact match from Companies search");
-          }
-          else {
-            company = companySearchResults[0];
-            console.log("Going with first result from Companies search");
-          };
+        //   // separate out this as function - duplicated in all bots
+        //   var exactMatch = companySearchResults.filter(function(eachCompany) {
+        //     return eachCompany.name.toLowerCase() === textInput.toLowerCase();
+        //   });
+        //   if (!companySearchResults.length) {
+        //     console.log("Nothing found in initial Company search");
+        //     return prepareResponse.nothingFound(responseObj);
+        //   }
+        //   else if (exactMatch && exactMatch.length) {
+        //     company = exactMatch[0];
+        //     console.log("Found an exact match from Companies search");
+        //   }
+        //   else {
+        //     company = companySearchResults[0];
+        //     console.log("Going with first result from Companies search");
+        //   };
 
-          // capture other company names for later use
-          var companyNames = companySearchResults.map(function(eachCompany) {
-            return eachCompany.name;
-          })
-          // filter out the textInput from companyNames
-          company.otherCompanies = companyNames.filter(function(name){
-            return name.toLowerCase() !== textInput.toLowerCase();
-          });
-          console.log("Other companies filtered from input:" + JSON.stringify(company.otherCompanies));
+        //   // capture other company names for later use
+        //   var companyNames = companySearchResults.map(function(eachCompany) {
+        //     return eachCompany.name;
+        //   })
+        //   // filter out the textInput from companyNames
+        //   company.otherCompanies = companyNames.filter(function(name){
+        //     return name.toLowerCase() !== textInput.toLowerCase();
+        //   });
+        //   console.log("Other companies filtered from input:" + JSON.stringify(company.otherCompanies));
 
-          // will format responseObj.payloads according to Company contents
-          return prepareResponse.loadCompanyToObj(responseObj, company);
-        });
+        //   // will format responseObj.payloads according to Company contents
+        //   return prepareResponse.loadCompanyToObj(responseObj, company);
+        // });
     }
     else if (event.postback) {
       let textInput = event.postback.payload;
       responseObj.context.textInput = textInput;
       console.log("Text input received from postback: " + textInput);
-      // summonResponse(responseObj, textInput);
-        return Q.when(companySearch.findAllByText(textInput))
-        .then(function (companySearchResults) {
-          // console.log("Company Search Results: " + JSON.stringify(companySearchResults).substring(0,200));
-          var company = {};
+      return summonResponse(responseObj, textInput);
+        // return Q.when(companySearch.findAllByText(textInput))
+        // .then(function (companySearchResults) {
+        //   // console.log("Company Search Results: " + JSON.stringify(companySearchResults).substring(0,200));
+        //   var company = {};
 
-          // separate out this as function - duplicated in all bots
-          var exactMatch = companySearchResults.filter(function(eachCompany) {
-            return eachCompany.name.toLowerCase() === textInput.toLowerCase();
-          });
-          if (!companySearchResults.length) {
-            console.log("Nothing found in initial Company search");
-            return prepareResponse.nothingFound(responseObj);
-          }
-          else if (exactMatch && exactMatch.length) {
-            company = exactMatch[0];
-            console.log("Found an exact match from Companies search");
-          }
-          else {
-            company = companySearchResults[0];
-            console.log("Going with first result from Companies search");
-          };
+        //   // separate out this as function - duplicated in all bots
+        //   var exactMatch = companySearchResults.filter(function(eachCompany) {
+        //     return eachCompany.name.toLowerCase() === textInput.toLowerCase();
+        //   });
+        //   if (!companySearchResults.length) {
+        //     console.log("Nothing found in initial Company search");
+        //     return prepareResponse.nothingFound(responseObj);
+        //   }
+        //   else if (exactMatch && exactMatch.length) {
+        //     company = exactMatch[0];
+        //     console.log("Found an exact match from Companies search");
+        //   }
+        //   else {
+        //     company = companySearchResults[0];
+        //     console.log("Going with first result from Companies search");
+        //   };
 
-          // capture other company names for later use
-          var companyNames = companySearchResults.map(function(eachCompany) {
-            return eachCompany.name;
-          })
-          // filter out the textInput from companyNames
-          company.otherCompanies = companyNames.filter(function(name){
-            return name.toLowerCase() !== textInput.toLowerCase();
-          });
-          console.log("Other companies filtered from input:" + JSON.stringify(company.otherCompanies));
+        //   // capture other company names for later use
+        //   var companyNames = companySearchResults.map(function(eachCompany) {
+        //     return eachCompany.name;
+        //   })
+        //   // filter out the textInput from companyNames
+        //   company.otherCompanies = companyNames.filter(function(name){
+        //     return name.toLowerCase() !== textInput.toLowerCase();
+        //   });
+        //   console.log("Other companies filtered from input:" + JSON.stringify(company.otherCompanies));
 
-          // will format responseObj.payloads according to Company contents
-          return prepareResponse.loadCompanyToObj(responseObj, company);
-        });
+        //   // will format responseObj.payloads according to Company contents
+        //   return prepareResponse.loadCompanyToObj(responseObj, company);
+        // });
     }
     else {
       console.log("Non-text-input Post detected from FB");
@@ -118,41 +118,41 @@ function getResponseObj(context) {
 }
 
 function summonResponse(responseObj, textInput) {
-  // return Q.when(companySearch.findAllByText(textInput))
-  // .then(function (companySearchResults) {
-  //   // console.log("Company Search Results: " + JSON.stringify(companySearchResults).substring(0,200));
-  //   var company = {};
+  return Q.when(companySearch.findAllByText(textInput))
+  .then(function (companySearchResults) {
+    // console.log("Company Search Results: " + JSON.stringify(companySearchResults).substring(0,200));
+    var company = {};
 
-  //   // separate out this as function - duplicated in all bots
-  //   var exactMatch = companySearchResults.filter(function(eachCompany) {
-  //     return eachCompany.name.toLowerCase() === textInput.toLowerCase();
-  //   });
-  //   if (!companySearchResults.length) {
-  //     console.log("Nothing found in initial Company search");
-  //     return prepareResponse.nothingFound(responseObj);
-  //   }
-  //   else if (exactMatch && exactMatch.length) {
-  //     company = exactMatch[0];
-  //     console.log("Found an exact match from Companies search");
-  //   }
-  //   else {
-  //     company = companySearchResults[0];
-  //     console.log("Going with first result from Companies search");
-  //   };
+    // separate out this as function - duplicated in all bots
+    var exactMatch = companySearchResults.filter(function(eachCompany) {
+      return eachCompany.name.toLowerCase() === textInput.toLowerCase();
+    });
+    if (!companySearchResults.length) {
+      console.log("Nothing found in initial Company search");
+      return prepareResponse.nothingFound(responseObj);
+    }
+    else if (exactMatch && exactMatch.length) {
+      company = exactMatch[0];
+      console.log("Found an exact match from Companies search");
+    }
+    else {
+      company = companySearchResults[0];
+      console.log("Going with first result from Companies search");
+    };
 
-  //   // capture other company names for later use
-  //   var companyNames = companySearchResults.map(function(eachCompany) {
-  //     return eachCompany.name;
-  //   })
-  //   // filter out the textInput from companyNames
-  //   company.otherCompanies = companyNames.filter(function(name){
-  //     return name.toLowerCase() !== textInput.toLowerCase();
-  //   });
-  //   console.log("Other companies filtered from input:" + JSON.stringify(company.otherCompanies));
+    // capture other company names for later use
+    var companyNames = companySearchResults.map(function(eachCompany) {
+      return eachCompany.name;
+    })
+    // filter out the textInput from companyNames
+    company.otherCompanies = companyNames.filter(function(name){
+      return name.toLowerCase() !== textInput.toLowerCase();
+    });
+    console.log("Other companies filtered from input:" + JSON.stringify(company.otherCompanies));
 
-  //   // will format responseObj.payloads according to Company contents
-  //   return prepareResponse.loadCompanyToObj(responseObj, company);
-  // });
+    // will format responseObj.payloads according to Company contents
+    return prepareResponse.loadCompanyToObj(responseObj, company);
+  });
 }
 
 // duplicate fcn in ./slack-handler (almost) - candidate for module
