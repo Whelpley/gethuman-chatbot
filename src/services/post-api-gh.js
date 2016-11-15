eaf06d06d0b0eb5004ad0dfe41a5f28b249d2f34
Whelpley <whelpley@gmail.com>
2016-11-15 14:42:29 -0500

    debug

--- src/services/post-api-gh.js
+++ src/services/post-api-gh.js
@@ -36,7 +36,7 @@ function findByCompany(company) {
         try {
           jsonBody = JSON.parse(body);
         } catch (ex) {
-          console.log('findByText parsing error for match ' + match + ' with body ' + body);
+          console.log('findByCompany parsing error for match with body: ' + body);
         }
       // console.log('Not-an-error after searching Posts of a Company!');
       deferred.resolve(jsonBody);
